8a46369211ab1cb2c211220d83f8185b9f6593eb
andreamaille <andrea.maille@gmail.com>
2019-09-13 15:20:30 -0400

    Basic pagination added

--- src/reducers/charactersReducer.js
+++ src/reducers/charactersReducer.js
@@ -1,6 +1,24 @@
 export default (state = [], action) => {
     switch (action.type) {
-        case 'FETCH_CHARACTERS': 
+        case 'FETCH_CHARACTERS':
+
+            // let characterResults = []
+
+            // for (let item of action.payload) {
+            //     const id = item.name.replace(/\s+/g, '')
+
+            //     characterResults = [
+            //             ...characterResults,
+            //             {[id]: item}
+            //     ]
+
+            // }
+
+            // return {
+            //     ...state, 
+            //     ...characterResults
+            // }
+
             return action.payload
         default:
             return state
