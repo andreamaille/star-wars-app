309385708f586a9ffcf18f45116dd91f13bc6511
andreamaille <andrea.maille@gmail.com>
2019-09-16 23:11:34 -0400

    Updates

---
+++ precache-manifest.570574949605ef7694fa80e0b7ff971b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a669a0cb96c8fb76eb2fea6c26332cd9",
+    "url": "/star-wars-app/index.html"
+  },
+  {
+    "revision": "3a8d0e3a45d0fb1093bb",
+    "url": "/star-wars-app/static/js/2.dae1ff6b.chunk.js"
+  },
+  {
+    "revision": "95d2f9378dbc1fe8dae6",
+    "url": "/star-wars-app/static/js/main.68d5f839.chunk.js"
+  },
+  {
+    "revision": "f04c688516313ff3e47e",
+    "url": "/star-wars-app/static/js/runtime~main.fe17d943.js"
+  }
+]);
